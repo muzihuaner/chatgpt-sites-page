ac393812dae7038898e95e043e91c6c17ec96403
muzihuaner <muzihuaner@hotmail.com>
2023-05-26 09:33:50 +0800

    gx

---
+++ assets/js/h5-common.js
@@ -0,0 +1,156 @@
+!(function () {
+  const isDev = ['localhost', '127.0.0', '192.168.'].some(d => location.host.includes(d));
+  const inited = {
+    comm: false,
+    bdtj: isDev,
+    iosDisableScale: false,
+  };
+
+  const h5Utils = {
+    config: {
+      h5List: [
+        // url, title, desc, type
+        ['/', '官方', '欢哥科技', 'l'],
+      ],
+    },
+    getUrlParams(search = location.search) {
+      const params = {};
+      const paramsSplit = search
+        .replace(/^[^\?]*\?/i, '')
+        .split(/&/)
+        .filter(d => d.trim());
+
+      if (paramsSplit.length > 0) {
+        paramsSplit.forEach(function (item) {
+          const list = item.split('=').map(d => decodeURIComponent(d));
+          const key = list.splice(0, 1);
+          const value = list.join('=');
+
+          if (params[key]) {
+            if (!Array.isArray(params[key])) params[key] = [params[key]];
+            params[key].push(value);
+          } else {
+            params[key] = value;
+          }
+        });
+      }
+
+      return params;
+    },
+    setRandomBodyBg(el = document.body) {
+      if (el) {
+        const bg = '//lzw.me/x/iapi/bing/api.php?n=8&idx=' + Math.ceil(Math.random() * 7);
+        if (el === 'blur') {
+          const id = 'bodyBefore';
+          const styleEl = document.getElementById(id);
+          if (styleEl) styleEl.remove();
+          setTimeout(() => {
+            document.head.insertAdjacentHTML(
+              'beforeend',
+              [
+                `<style id="${id}">body:before{`,
+                `content: ''; position: fixed; top: 0; left: 0; width: 100%; height: 100%;`,
+                `background: transparent url(https://lzw.me/x/iapi/bing/api.php?n=10) center/cover no-repeat;`,
+                `filter: blur(7px); z-index: -1; background-attachment: fixed;`,
+                `}</style>`,
+              ].join('')
+            );
+          }, 0);
+        } else if (el.setAttribute) {
+          el.setAttribute('style', `background: transparent url(${bg}) center/cover no-repeat fixed`);
+        }
+      }
+    },
+    loadJsOrCss(urls = []) {
+      if (typeof urls == 'string') urls = [urls];
+      const list = [];
+      for (const url of urls) {
+        const isCss = url.includes('.css');
+        const isLoaded = document.querySelector(isCss ? `link[href="${url}"]` : `script[src="${url}"]`);
+        if (isLoaded) continue;
+
+        const el = document.createElement(isCss ? 'link' : 'script');
+        if (isCss) {
+          el.rel = 'stylesheet';
+          el.href = url;
+        } else {
+          el.src = url;
+          // el.type = 'module';
+        }
+
+        list.push(
+          new Promise(rs => {
+            el.onload = () => rs();
+            setTimeout(() => rs(), 10_000);
+            document.querySelector('head').append(el);
+          })
+        );
+      }
+
+      return Promise.allSettled(list);
+    },
+    loadBDLianmeng(force) {
+      if (force || window.slotbydup) {
+        this.loadJsOrCss(`//cpro.baidustatic.com/cpro/ui/cm.js`);
+      }
+    },
+    /** {@see https://sweetalert2.github.io/} */
+    alert(...args) {
+      return this.loadJsOrCss([
+        'https://npm.elemecdn.com/sweetalert2@11/dist/sweetalert2.min.css',
+        'https://npm.elemecdn.com/sweetalert2@11/dist/sweetalert2.all.min.js',
+      ]).then(() => Swal.fire(...args));
+    },
+  };
+
+  
+
+  /** ios 禁用缩放 */
+  function iosDisableScale() {
+    if (inited.iosDisableScale) return;
+    inited.iosDisableScale = true;
+
+    if (/iPad｜Macintosh|iPhone OS/i.test(navigator.userAgent)) {
+      document.addEventListener('gesturestart', function (event) {
+        event.preventDefault();
+      });
+    }
+  }
+
+  function setCurrentYear() {
+    const cy = document.getElementById('currentYear');
+    if (cy) cy.innerText = new Date().getFullYear();
+  }
+
+  function loadCommCss() {
+    return h5Utils.loadJsOrCss(`https://lzw.me/x/lib/utils/h5-common.css?v=1`); // ${new Date().toISOString().slice(0, 10)}
+  }
+
+  // === init ====
+  function h5CommInit(types, isMergeDefault = true) {
+    const defaultTypes = ['disableScale', 'bdtj'];
+
+    if (!types) types = defaultTypes;
+    else if (isMergeDefault) types = defaultTypes.concat(types);
+
+    setCurrentYear();
+
+ 
+    if (types.includes('disableScale')) iosDisableScale();
+    if (types.includes('bg')) h5Utils.setRandomBodyBg();
+    if (types.includes('css')) loadCommCss();
+
+    inited.comm = true;
+  }
+
+  function init() {
+    window.h5Utils = h5Utils;
+    window.h5CommInit = h5CommInit;
+
+    setTimeout(() => {
+      if (!inited.comm) h5CommInit();
+    }, 1000);
+  }
+
+  init();
+})();
---
+++ assets/js/main.js
@@ -0,0 +1,223 @@
+'use strict';
+
+const urlParams = h5Utils.getUrlParams();
+const CS = {
+  keyword: urlParams.d || '',
+  type: urlParams.type || 'all',
+  el: {
+    searchBar: document.getElementById('searchBar'),
+    btnGroup: document.getElementById('btnGroup'),
+    searchInput: document.getElementById('search'),
+    total: document.getElementById('total'),
+    siteList: document.getElementById('siteList'),
+    backToTo: document.querySelector('.scroll-fixed'),
+  },
+  allList: [],
+  info: {
+    recommend: [],
+    needKey: [],
+    needPay: [],
+    needPwd: [],
+    needVPN: [],
+    needVerify: [],
+    invalid: [],
+  },
+};
+const typeKeys = Object.keys(CS.info);
+
+function htmlEscape(str) {
+  return String(str || '').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+}
+
+function getSiteList() {
+  return fetch('https://ghp.quickso.cn/https://raw.githubusercontent.com/muzihuaner/chatgpt-sites/main/site-info.json')
+    .then((d) => d.json())
+    .then((d) => {
+      if (d.siteInfo) {
+        const list = Object.entries(d.siteInfo)
+          .filter((d) => !d[1].hide)
+          .sort((a, b) => {
+            for (const key of ['invalid', 'needVerify', 'needVPN', 'needPwd', 'needPay', 'needKey']) {
+              if ('needVerify' == key) {
+                if (a[1].needVerify > 2 || b[1].needVerify > 2) return a[1].needVerify > 2 ? 1 : -1;
+              } else if (a[1][key] !== b[1][key]) return a[1][key] ? 1 : -1;
+            }
+
+            if (a[1].star !== b[1].star) return (b[1].star || 1) - (a[1].star || 1);
+
+            return a[0] > b[0] ? 1 : -1;
+          });
+
+        // format
+        for (const [url, item] of list) {
+          Object.entries(item).forEach(([key, val]) => {
+            if (val && typeof val === 'string') item[key] = htmlEscape(val);
+          });
+
+          item.url = url;
+          if (!item.title) item.title = url.replace(/https?:\/\//, '');
+          if (!item.desc && !item.errmsg) item.desc = url;
+
+          let typed = 0;
+          typeKeys.forEach((key) => {
+            if (item[key]) {
+              if (key === 'needVerify' && item.needVerify < 1) return;
+
+              CS.info[key].push(item);
+              typed = 1;
+            }
+          });
+
+          if (!typed) {
+            item.recommend = 1;
+            CS.info.recommend.push(item);
+          }
+
+          setPrefixAndIcon(item);
+        }
+
+        CS.alllist = list.map((d) => d[1]);
+        return list;
+      }
+    });
+}
+
+function setPrefixAndIcon(info) {
+  let prefix = '';
+
+  if (info.invalid) prefix = '❌' + (typeof info.invalid === 'string' ? info.invalid : '');
+  else if (info.needVerify > 2) prefix += '❓';
+
+  if (info.star && info.star > 0) prefix += '⭐'.repeat(Math.min(3, info.star));
+  else if (info.star == 0) prefix += '⛔';
+
+  if (info.needPwd) prefix += '🔒';
+  if (info.needPay) prefix += '💰';
+  if (info.needKey) prefix += '🔑';
+  if (info.needVPN) prefix += '🚀';
+
+  // cls、icon
+  if (info.recommend) {
+    info.cls = 'item-green';
+    if (!info.icon) info.icon = 'fa-star';
+  } else if (info.invalid) {
+    info.cls = 'item-red';
+    if (!info.icon) info.icon = 'fa-solid fa-xmark';
+  } else if (info.needVerify > 0) {
+    info.cls = 'item-purple';
+    if (!info.icon) info.icon = 'fa-regular fa-circle-question';
+  } else if (info.needPwd) {
+    info.cls = 'item-primary';
+    if (!info.icon) info.icon = 'fa-solid fa-unlock-keyhole';
+  } else if (info.needPay) {
+    info.cls = 'item-blue';
+    if (!info.icon) info.icon = 'fa-brands fa-amazon-pay';
+  } else if (info.needKey) {
+    info.cls = 'item-pink';
+    if (!info.icon) info.icon = 'fa-solid fa-key';
+  } else if (info.needVPN) {
+    info.cls = 'item-orange';
+    if (!info.icon) info.icon = 'fa-solid fa-paper-plane';
+  } else {
+    info.cls = 'item-purple';
+    if (!info.icon) info.icon = 'fa-regular fa-star';
+  }
+
+  info.prefix = prefix;
+  if (!info.star) info.star = 1;
+}
+
+function updateElList(type = CS.type) {
+  const htmlList = [];
+  const list = CS.info[type] || CS.alllist;
+
+  list.forEach((item, idx) => {
+    if (CS.keyword) {
+      const ok = [item.url, item.desc, item.title, item.errmsg].some((d) => d && d.includes(CS.keyword));
+      if (!ok) return;
+    }
+
+    htmlList.push(` <div class="item ${item.cls || 'item-green'} col" title="${item.desc || item.title}">
+		<div class="item-inner">
+			<div class="icon-holder">
+				<i class="icon fa ${item.icon || 'paper-plane'}"></i>
+			</div>
+			<h3 class="title d-flex align-items-center">${item.title}</h3>
+			<p class="intro">${item.prefix || '⭐'} ${item.desc || ''} <span class="errmsg text-danger">${item.errmsg || ''}</span></p>
+			<a class="link" href="${item.url}" target="_blank"><span>${idx + 1}</span></a>
+		</div>
+	</div>`);
+  });
+
+  CS.el.total.innerHTML = htmlList.length;
+  CS.el.siteList.innerHTML = htmlList.join('');
+}
+
+function onInputChange(event) {
+  const keyword = event.target.value.trim();
+  console.log(keyword, event)
+  if (keyword !== CS.keyword) {
+    CS.keyword = keyword;
+    updateElList();
+
+    const url = new URL(location.href);
+    if (!keyword) url.searchParams.delete('w');
+    else url.searchParams.set('w', keyword);
+    history.pushState(null, null, url.href);
+  }
+}
+
+async function init() {
+  (window.slotbydup = window.slotbydup || []).push({
+      id: document.body.clientWidth < 1000 ?  'u6898755' : 'u6899146', // 'u6899145',
+      container: "_e12r7e6qdvu",
+      async: true
+  });
+  h5Utils.loadJsOrCss(`//cpro.baidustatic.com/cpro/ui/cm.js`);
+
+  await getSiteList();
+  updateElList();
+
+  CS.el.searchInput.addEventListener('change', onInputChange, false);
+  CS.el.searchInput.addEventListener('input', onInputChange, false);
+  CS.el.btnGroup.addEventListener(
+    'click',
+    () => {
+      const type = document.querySelector('input[name=btnradio]:checked').getAttribute('id');
+      if (type && CS.type !== type) {
+        CS.type = type;
+        updateElList(type);
+
+        const url = new URL(location.href);
+        if (type === 'type') url.searchParams.delete('type');
+        else url.searchParams.set('type', type);
+        history.pushState(null, null, url.href);
+      }
+    },
+    false
+  );
+
+  const searchTop = CS.el.searchBar.getBoundingClientRect().top;
+
+  window.onscroll = function () {
+    const maxtop = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if (maxtop > searchTop) {
+      CS.el.backToTo.style.display = 'flex';
+      CS.el.searchBar.classList.add('search-bar__fixed');
+    } else {
+      CS.el.backToTo.style.display = 'none';
+      CS.el.searchBar.classList.remove('search-bar__fixed');
+    }
+  };
+
+  document.querySelector('.scroll-fixed .scroll-bottom').addEventListener(
+    'click',
+    (ev) => {
+      ev.preventDefault();
+      window.scrollBy(0, document.body.scrollHeight);
+    },
+    false
+  );
+}
+
+init();
